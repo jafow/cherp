4a24aabca7e2c8589b9933d16b98b03458341513
jafow <jared.a.fowler@gmail.com>
2020-07-17 21:05:01 -0700

    add --version option
    error if missing org

--- src/usage.js
+++ src/usage.js
@@ -1,8 +1,10 @@
-module.exports = usage
+module.exports = {usage, version}
+
+const packagejson = require('../package.json')
 
 function usage () {
-  console.log('🐦 cherp 🐦 - a tool for CHecking github RePos')
-  console.log('Usage: ')
+  console.log('🐦 cherp 🐦 - a tool for Checking GitHub repos')
+  console.log('\nUsage: ')
   console.log(`
     $ cherp -h
       show this help output
@@ -10,4 +12,8 @@ function usage () {
     $ cherp add-file --license=GPL-2.0 --repo=my-repo
       opens a PR to add a GPL-2.0 license file to "my-repo"
   `)
+}
+
+function version () {
+  console.log(`cherp: ${packagejson.version}`)
 }
